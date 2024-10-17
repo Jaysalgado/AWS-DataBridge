7dcb356499a3d1d7968b7b0613c29a792040dffa
Aban <khanaban505@gmail.com>
2024-10-17 17:54:17 -0400

    doc/nep done?

--- src/import/neptune.py
+++ src/import/neptune.py
@@ -1,42 +1,45 @@
-# neptune uses Gremlin (property graphs) or SPARQL (RDF data and semantic applications)
-# relational != graph 
-# need to transform relational data into graph data
-# need to decide which columns represent nodes (entities) and which are edges (relationships)
-
-# property graphs good fit for health-data because they model complex data
-
-# identify nodes/vertex
-
-# each entity indicated by the primary key is a node, the rest of the tuples
-# indicate the "edges" or relationships 
-
-# 1) ask if one csv or two csv (node/edge)
-# 2) ask for primary key column, if none programmatically add one, make that node
-# 3) push to db
-
 import csv
 import pandas as pd
 import boto3
 
-data = "./data/diabetes_data.csv" # Path to data file
+data = "./data/diabetes_data.csv"
 
-d_frame = pd.read_csv(data) # Setting data frame variable
+try:
+    d_frame = pd.read_csv(data)
+    print(f"Data loaded from {data}")
+except FileNotFoundError:
+    print("File not found.")
+    exit(1)
+except pd.errors.EmptyDataError:
+    print("The provided file is empty. Please load appropriate file.")
 
-node_id = input("Which column would you like to use as the unique identifier for the nodes? ") # Identifying node column
-property_id = input("What are the column names you would like to use as the properties for your nodes?  ") # Identifying property columns
-relationship_id = input("Enter the column names that represent relationships, no name is fine: ") # Identifying relationship columns between properties
+node_id = input("Which column would you like to use as the unique identifier for the nodes? ")
+if node_id not in d_frame.columns:
+    print(f"{node_id} not found in data.")
+    exit(1)
 
-vertex_export = "/mnt/data/vertex.csv" # Temporary placeholder for csv files generated, will be pushed to Loader with Boto3
-edges_export = "/mnt/data/edges.csv" # Temporary placeholder for csv files generated, will be pushed to Loader with Boto3
+if d_frame[node_id].isnull().any():
+    print("The column you identified contains null values. Please rectify.")
+    exit(1)
+
+property_id = input("What are the column names you would like to use as the properties for your nodes?  ")
+if property_id not in d_frame.columns:
+    print(f"{property_id} not found in data.")
+    exit(1)
+
+relationship_id = input("Enter the column names that represent relationships, no name is fine: ")
+
+vertex_export = "/mnt/data/vertex.csv"
+edges_export = "/mnt/data/edges.csv"
 
 with open(vertex_export, 'w', newline='') as v, open(edges_export, 'w', newline='') as e:
-    vertex_mod = csv.writer(v) # Sets write for vertex
-    edge_mod = csv.writer(e) # Sets write for edge
+    vertex_mod = csv.writer(v)
+    edge_mod = csv.writer(e)
 
     edge_id = 1
 
-    vertex_mod.writerow(['~id', '~label'] + relationship_id) # Headers for vertex, as indicated by AWS documentation
-    edge_mod.writerow(['~id', '~from', '~to', '~label']) # Headers for edge, as indicated by AWS documentation
+    vertex_mod.writerow(['~id', '~label'] + relationship_id)
+    edge_mod.writerow(['~id', '~from', '~to', '~label'])
 
     for index, row in d_frame.iterrows():
         node_id_trace = f'{row[node_id]}'
@@ -57,12 +60,19 @@ print('Vertices and edges CSV files have been written.')
 client_container = boto3.client('s3') 
 import_bucket = 'import-bucket'
 
-client_container.upload_file(vertex_export, import_bucket, 'vertex.csv')
-client_container.upload_file(edges_export, import_bucket, 'edges.csv')
+try: 
+    client_container.upload_file(vertex_export, import_bucket, 'vertex.csv')
+    print(f"{vertex_export} uploaded to S3 bucket: {import_bucket}")
+    client_container.upload_file(edges_export, import_bucket, 'edges.csv')
+    print(f"{edges_export} uploaded to S3 bucket: {import_bucket}")
+except boto3.exceptions.S3UploadFailedError as aws_err:
+    print(f"Upload error: {aws_err}")
+except boto3.exceptions.NoCredentialsError as auth_err:
+    print(f"Credentials error: {auth_err}")
 
 neptune = boto3.client('neptune-db')
 
-def neptune_bulk_loader_call(s3_uri):
+def nep_load(s3_uri):
     response = neptune.start_loader_job(
         source = s3_uri,
         format = 'csv',
@@ -70,14 +80,13 @@ def neptune_bulk_loader_call(s3_uri):
         region = 'my-region',
         failOnError = True
     )
-    
     return response
 
 vertex_s3_uri = f's3://{import_bucket}/vertex.csv'
 edges_s3_uri = f's3://{import_bucket}/edges.csv'
 
-response_vertices = neptune_bulk_loader_call(vertex_s3_uri)
+response_vertices = nep_load(vertex_s3_uri)
 print("Neptune Load for Vertex:", response_vertices)
 
-response_edges = neptune_bulk_loader_call(edges_s3_uri)
+response_edges = nep_load(edges_s3_uri)
 print("Neptune Load for Edges:", response_edges)
