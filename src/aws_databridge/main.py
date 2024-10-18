017f1c587a6eca029f2ca003b2084d24b9a6beb2
Aban <khanaban505@gmail.com>
2024-10-18 18:25:24 -0400

    aban commit

--- src/aws_databridge/main.py
+++ src/aws_databridge/main.py
@@ -1,31 +1,35 @@
-from aws_databridge.utils.convert_to import text_to_csv, json_to_csv, xml_to_csv
-from aws_databridge.db_import.rds import import_to_rds
-from aws_databridge.db_import.dynamodb import import_to_dynamodb
-from aws_databridge.db_import.document import import_to_documentdb
-from aws_databridge.db_import.neptune import import_to_neptune
+from utils.convert_to import text_to_csv, json_to_csv, xml_to_csv
+from db_import.rds import import_to_rds
+from db_import.dynamodb import import_to_dynamodb
+from db_import.document import import_to_documentdb
 import os
 
 def main():
     file = input('Enter the full name of the file (e.g. animals.txt , include folder path if applicable-> folder/animals.txt):')
-    file = os.path.join('/home/ec2-user/', file)
+    file = os.path.join('/home/ec2-user/AWS-DataBridge', file)
     file_type = file.split('.')[1]
 
     db_choice = input('What database would you like to import this data to? (rds = 1, dynamo = 2, neptune = 3, documentDB = 4): ')
     table_name = input('Enter the name of the table you would like to import this data to: ')
 
+    csv_check = input('Would you like to convert the file to CSV? 1 = Yes, 2 = No')
     #convert supported non csv types to csv
-    if file_type == 'txt':
-        print('Converting data to csv...')
-        text_to_csv(file)
-        print('Data has been converted to csv')
-    elif file_type == 'json':
+
+    if csv_check == '1':
+        if file_type == 'txt':
             print('Converting data to csv...')
-            json_to_csv(file)
+            text_to_csv(file)
             print('Data has been converted to csv')
-    elif file_type == 'xml':
-            print('Converting data to csv...')
-            xml_to_csv(file)
-            print('Data has been converted to csv')
+        elif file_type == 'json':
+                print('Converting data to csv...')
+                json_to_csv(file)
+                print('Data has been converted to csv')
+        elif file_type == 'xml':
+                print('Converting data to csv...')
+                xml_to_csv(file)
+                print('Data has been converted to csv')
+    else:
+        print('CSV conversion skipped.')
 
     # import data to the selected database, allowing for multiple imports if neccessary 
     stop = 0;
